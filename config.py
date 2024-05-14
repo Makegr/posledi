8ed240742b1b4245dbf732b1e402fa3a48c8dc97
Makegr <psgepm@gmail.com>
2024-05-14 18:58:03 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,21 @@
+TELEGRAM_TOKEN = '6721907014:AAFONnZA_rUs8OOyRPGgsD2fGpkLsqO5gFY'
+FOLDER_ID = 'b1geu00m4ve8plpvh2tt'
+IAM_TOKEN = 't1.9euelZqNjJrKl4mJi56cjpWUypyLiu3rnpWanZuKkc6WzYzLz5qWmp7Jx4_l8_d4SHJN-e9ZD3YE_t3z9zh3b03571kPdgT-zef1656VmpSKy5LHzJrPnIyJx8uby5mM7_zF656VmpSKy5LHzJrPnIyJx8uby5mMveuelZqci87OlJmQkpCKnImdlJPPybXehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.i25SQVuE8rTWObxPvq68NPj9lOLcO8r2h_v_YzJd7zLNjuOf7JvY6FhCi85C-Hr7bTyc5EzsXmyyL0NpyQAsBg'
+HOME_DIR = 'C:/Users/psgep/PycharmProjects/posledi'
+IAM_TOKEN_PATH = f'{HOME_DIR}/creds/iam_token.json'  # файл для хранения iam_token
+FOLDER_ID_PATH = f'{HOME_DIR}/creds/folder_id.json'  # файл для хранения folder_id
+BOT_TOKEN_PATH = f'{HOME_DIR}/creds/bot_token.json'  # файл для хранения bot_token
+
+COUNT_LAST_MSG = 4
+MAX_USER_TTS_SYMBOLS = 1000
+MAX_TTS_SYMBOLS = 200
+MAX_USER_STT_BLOCKS = 12
+MAX_USERS = 3
+MAX_GPT_TOKENS = 120
+MAX_USER_GPT_TOKENS = 2_000
+SYSTEM_PROMPT = [{'role': 'system',
+                  'text': 'Ты умный и дружелюбный собеседник. '
+                          'Общайся с пользователем на "ты" и используй юмор.'
+                          'Поддерживай диалог. Изображай человека'}]
+LOGS = 'logs.txt'
+PATH_TO_DB = 'messages.db'
